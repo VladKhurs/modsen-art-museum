1e823a66d2d739d322b8944cb7f04436be3e51c8
Vladislav Khurs <130601125+VladKhurs@users.noreply.github.com>
2024-10-18 18:23:05 +0300

    feat: init router (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import './App.scss';
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '@/pages/Home/Home'
+import DetailInfo from '@/pages/DetailInfo/DetailInfo'
+import Favorites from '@/pages/Favorites/Favorites'
+import Header from '@/components/Header/Header'
+import Footer from '@/components/Footer/Footer'
 
 const App: React.FC = () => 
-    <div className="App">
-        Museum of Art
-    </div>;
+    <BrowserRouter>
+        <Header/>
+        <Routes>
+            <Route path="*" element={<Home />} />
+            <Route path="detail-info" element={<DetailInfo />} />
+            <Route path="favorites" element={<Favorites />} />
+        </Routes>
+        <Footer/>
+    </BrowserRouter>
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import './Header.scss'
+import React from 'react';
+const Header: React.FC = () => (
+    <header>
+        Header
+    </header>
+);
+
+export default Header;
