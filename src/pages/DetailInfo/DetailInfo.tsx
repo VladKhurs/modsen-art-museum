e0ead3f9d5e4818a9d435d5db8b488bc8bf2f597
Vladislav Khurs <130601125+VladKhurs@users.noreply.github.com>
2024-10-18 16:27:59 +0300

    feat: init pages (#2)

---
+++ src/pages/DetailInfo/DetailInfo.tsx
@@ -0,0 +1,9 @@
+import './DetailInfo.scss'
+import React from 'react';
+const DetailInfo: React.FC = () => (
+    <section>
+        DetailInfo
+    </section>
+);
+  
+export default DetailInfo;
---
+++ src/pages/Favorites/Favorites.tsx
@@ -0,0 +1,9 @@
+import './Favorites.scss'
+import React from 'react';
+const Favorites: React.FC = () => (
+    <section>
+        Favorites
+    </section>
+);
+  
+export default Favorites;
