0d8099793d1e6eeffde9c3ec677844eca32671a7
Vladislav Khurs <khursvladislav@gmail.com>
2024-10-22 19:49:13 +0300

    feat: add debounce

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,8 +1,25 @@
-import { Card } from "@/constants/types";
+import { Card } from '@/constants/types';
+import { useEffect, useState } from 'react';
 
 export const addUnique = (arr: Card[], newItem: Card) => {
-    if (!arr.some(item => item.id === newItem.id)) {
-        arr.push(newItem);
-        sessionStorage.setItem('favorites', JSON.stringify(arr));
-    }
+	if (!arr.some((item) => item.id === newItem.id)) {
+		arr.push(newItem);
+		sessionStorage.setItem('favorites', JSON.stringify(arr));
+	}
 };
+
+export function useDebounce(value: string, delay: number) {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [value, delay]);
+
+    return debouncedValue;
+}
