0c6adfdc7dad4ee3dc847fd5eebf092394802884
Vladislav Khurs <khursvladislav@gmail.com>
2024-10-23 19:43:36 +0300

    feat: add adaptive

--- src/components/UI/Pagination/Pagination.tsx
+++ src/components/UI/Pagination/Pagination.tsx
@@ -6,7 +6,9 @@ import arrowRight from '@/assets/arrow-right.svg';
 import { ContextProps } from '@/constants/types';
 
 const Pagination: React.FC = () => {
-	const { setIsLoading, page, setPage, limit, setLimit } = useContext(Context) as ContextProps;
+	const { setIsLoading, page, setPage, limit, setLimit } = useContext(
+		Context
+	) as ContextProps;
 	const [dropdownOpen, setDropdownOpen] = useState(false);
 
 	const handleDropdownClick = () => {
@@ -21,43 +23,47 @@ const Pagination: React.FC = () => {
 				</button>
 				{dropdownOpen && (
 					<div>
-						<button onClick={() => {
-							setLimit(limit === 5 ? 3 : 5)
-							setDropdownOpen(false);
-							setIsLoading(true);
-						}}>
+						<button
+							onClick={() => {
+								setLimit(limit === 5 ? 3 : 5);
+								setDropdownOpen(false);
+								setIsLoading(true);
+							}}
+						>
 							Paginate by {limit === 5 ? 3 : 5}
 						</button>
 					</div>
 				)}
 			</div>
 
-			{page !== 1 ? (
+			<div className="paginate">
+				{page !== 1 ? (
+					<button
+						className="arrow-left"
+						onClick={() => {
+							setPage(page - 1);
+							setIsLoading(true);
+						}}
+					>
+						<img src={arrowLeft} alt="" />
+					</button>
+				) : (
+					<></>
+				)}
+				<button className="number number-special">{0 + page}</button>
+				<button className="number">{1 + page}</button>
+				<button className="number">{2 + page}</button>
+				<button className="number">{3 + page}</button>
 				<button
-					className="arrow-left"
+					className="arrow-right"
 					onClick={() => {
-						setPage(page - 1);
+						setPage(page + 1);
 						setIsLoading(true);
 					}}
 				>
-					<img src={arrowLeft} alt="" />
+					<img src={arrowRight} alt="" />
 				</button>
-			) : (
-				<></>
-			)}
-			<button className="number number-special">{0 + page}</button>
-			<button className="number">{1 + page}</button>
-			<button className="number">{2 + page}</button>
-			<button className="number">{3 + page}</button>
-			<button
-				className="arrow-right"
-				onClick={() => {
-					setPage(page + 1);
-					setIsLoading(true);
-				}}
-			>
-				<img src={arrowRight} alt="" />
-			</button>
+			</div>
 		</div>
 	);
 };
